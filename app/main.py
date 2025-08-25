fd84f6cbf579fe91500bec4437506de9eef86b25
miguelamaral254 <miguel.amaral.sales@gmail.com>
2025-08-25 18:08:00 -0300

    Feat: cors

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware  # Importe o middleware
 from app.domain.md_vagas import vaga_controller
 from app.domain.md_talentos import talento_controller
 from app.domain.md_ia.model_loader import model_loader
@@ -8,6 +9,20 @@ app = FastAPI(
     title="API de Análise de Talentos",
     description="Sistema para gerenciar vagas e ranquear talentos usando IA.",
     version="1.2.0"
+)
+
+# Adicione o middleware de CORS aqui
+origins = [
+    "http://localhost:5173",  # A URL do seu frontend Vite
+    "http://127.0.0.1:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos os métodos HTTP (GET, POST, etc.)
+    allow_headers=["*"],  # Permite todos os cabeçalhos
 )
 
 @app.on_event("startup")
